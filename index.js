1374c3fb7054cc427922c9927e97f77a4911c327
Vinitjp123 <vinitjpanchal2002@gmail.com>
2023-09-26 19:22:53 +0530

    experimented apigateways

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const express = require('express');
 const morgan = require('morgan');
+const {createProxyMiddleware} = require('http-proxy-middleware');
+const { rateLimit } = require('express-rate-limit');
+const { default: axios } = require('axios');
+
 const app = express();
 const PORT = 3004;
+const limiter = rateLimit({
+    windowMs:2 * 60 * 1000,
+    max:5
+});
 
 app.use(morgan('combined'));
+//app.use(limiter);
 
+app.use('/bookingservice',async (req,res,next)=>{
+    console.log(req.headers['x-access-token']);
+    try {
+        const response = await axios.get('http://localhost:3001/api/v1/isAuthenticated',{
+            headers:{
+                'x-access-token': req.headers['x-access-token']
+            }
+        });
+    console.log(response.data);
+    if(response.data.success){
+        next();
+    }else{
+        return res.status(401).json({
+            message:'unauthorised'
+        });
+    }
+    
+    } catch (error) {
+        return res.status(401).json({
+            message:'something wrong'
+        });
+    }
+})
+app.use('/bookingservice', createProxyMiddleware({ target: 'http://localhost:3002' }));
 app.get('/home',(req,res)=>{
     return res.json({message:'Ok'});
 });
